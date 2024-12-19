7ca495b19608879bf2bfcb76dce350996ba76907
Per Brendelökken <per.brendelokken@afry.com>
2024-12-19 07:46:13 +0100

    Day 19, part 1 only

--- src/a19/run.rs
+++ src/a19/run.rs
@@ -1,5 +1,6 @@
-
-use regex::Regex;
+use std::cell::RefCell;
+use std::collections::{HashMap, HashSet};
+use itertools::Itertools;
 
 fn main() {
     let mut lines = Vec::<&str>::new();
@@ -8,15 +9,77 @@ fn main() {
         .split("\n")
         .for_each(|line| lines.push(line));
 
+    let towel_types: Vec<String> = lines[0].split(",")
+        .map(|t| t.trim().to_string())
+        .sorted_by_key(|t| (t.len() as i64))
+        .collect();
 
-    let re = Regex::new(r"\w\w\w(?<name>\w+)").unwrap();
+    let combined_towels = &lines[2..lines.len()];
 
-    for line in lines {
-        let Some(capture) = re.captures(line) else {
-            println!("no match");
-            return;
+    println!("{:?} {:?}", &towel_types.len(), &combined_towels.len());
+
+
+
+    // let count = combined_towels.iter()
+    //     .filter(|combined_towel| is_possible_pattern(combined_towel, &towel_types))
+    //     .count();
+
+    //panic!();
+
+    let mut count = 0;
+    for combined_towel in combined_towels {
+        println!("todo {combined_towel}");
+        let cache = Cache {
+            impossible_patterns: RefCell::new(HashSet::new())
         };
-        println!("capture: {:?}", capture.name("name"));
+        if is_possible_pattern(combined_towel, &towel_types,0, &cache) {
+            println!("possible {combined_towel}");
+            count += 1;
+        } else {
+            println!("not possible {combined_towel}");
+        }
     }
 
+    println!("part 1: {count}");
+    //400
+}
+
+struct Cache {
+    impossible_patterns: RefCell<HashSet<String>>,
+}
+
+static mut a: usize = 0;
+
+fn is_possible_pattern(pattern: &str, towels: &Vec<String>, level: usize, cache: &Cache) -> bool {
+
+    unsafe {
+        a += 1;
+        if a % 1000000 == 0 {
+            println!("{a}");
+        }
+    }
+
+
+    if cache.impossible_patterns.borrow().contains(pattern) {
+        print!("x");
+        return false;
+    }
+
+    let mut is_possible = false;
+    for t in towels {
+        if pattern.starts_with(t) {
+            let sub_pattern = &pattern[t.len().. pattern.len()];
+            if level <= 4 {
+                println!("level {level} {t}");
+            }
+            if sub_pattern.is_empty() || is_possible_pattern(sub_pattern, towels, level + 1, cache) {
+                is_possible = true;
+                break;
+            }
+        }
+    }
+    if !is_possible {
+        cache.impossible_patterns.borrow_mut().insert(pattern.to_string());
+    }
+    is_possible
 }
